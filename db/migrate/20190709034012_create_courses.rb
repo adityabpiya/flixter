5a3767384ba118aea9f412378f1a185a6682a78b
Aditya Bhasu Piya <aditya.b.piya@gmail.com>
2019-07-09 04:40:01 +0000

    solved the issue of lesson 13 typo error

--- db/migrate/20190709034012_create_courses.rb
+++ db/migrate/20190709034012_create_courses.rb
@@ -4,7 +4,7 @@ class CreateCourses < ActiveRecord::Migration[5.2]
     	t.string :title
     	t.text :description
     	t.decimal :cost
-    	t.integet :user_id
+    	t.integer :user_id
     	t.timestamps
 
       
