aeb8725e5346bcb8175406fbef86e5c92f98ae0e
Aditya Bhasu Piya <aditya.b.piya@gmail.com>
2019-07-19 00:26:03 +0000

    completed lesson 28 enrollment permission

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,9 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+  	before_action :require_enrollment_for_current_course, only: [:show]
+
+
+
 	def show
 	end
 
@@ -8,4 +13,12 @@ class LessonsController < ApplicationController
 	def current_lesson
 		@current_lesson ||= Lesson.find(params[:id])
 	end
+
+	private
+
+		def require_enrollment_for_current_course
+			if !current_user.enrolled_in?(current_lesson.section.course)
+				redirect_to course_path(current_lesson.section.course), alert: "Please Enroll to access lessons!"
+			end
+		end
 end
